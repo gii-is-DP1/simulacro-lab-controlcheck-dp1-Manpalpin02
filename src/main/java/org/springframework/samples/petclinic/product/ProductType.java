7ca539858e11749ecb37b08fdf86829b3508ad94
Manpalpin02 <manuel18602@gmail.com>
2022-11-03 01:12:16 +0100

    Prueba push

--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -2,10 +2,19 @@ package org.springframework.samples.petclinic.product;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.samples.petclinic.model.BaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 @Getter
 @Setter
-public class ProductType {
-    Integer id;
+public class ProductType extends BaseEntity {
+
+    @Column(unique = true)
+    @Size(min = 3, max = 50)
     String name;
 }
